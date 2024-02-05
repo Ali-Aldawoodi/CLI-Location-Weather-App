5f767c08416748928a6af9acdd32fba0fab225fc
ali <alialdawoodi99@yahoo.com>
2024-02-05 10:03:53 -0700

    Added a display and main methods in order to properly display to the CLI

--- script.rb
+++ script.rb
@@ -45,3 +45,43 @@ require 'uri'
        
     
       end
+
+      
+    def display_weather(city_name, latitude, longitude, current_temp, daily_forecast)
+        puts "City Name: #{city_name}"
+        puts "Latitude: #{latitude}"
+        puts "Longitude: #{longitude}"
+        puts "Current Weather #{city_name}"
+        puts "Current Temperature: #{current_temp} °F"
+  
+        puts "7-Day Forecast"
+        dates = daily_forecast["time"]
+        
+        daily_forecast['time'].each_with_index do |date, index|
+          puts "Date: #{date}"
+          puts "High Temperature: #{daily_forecast['temperature_2m_max'][index]} °F"
+          puts "Low Temperature: #{daily_forecast['temperature_2m_min'][index]} °F"
+      
+      end
+      end
+  
+      def main
+        puts "Welcome!"
+         print "Enter your city: "
+         city_name = gets.chomp.strip
+        coordinates = fetch_city(city_name)
+  
+  
+        if coordinates 
+          latitude, longitude = coordinates
+  
+          current_temp, daily_forecast = fetch_weather(latitude, longitude)
+  
+          if current_temp && daily_forecast
+            display_weather(city_name, latitude, longitude, current_temp, daily_forecast)
+          end
+        end
+      end
+      main
+  
+      
